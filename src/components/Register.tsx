d6453873e15c12810ee03723e40780f4107bed10
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-13 07:34:56 +0530

    Replace the input elements with the material ui element textfield

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,15 +1,18 @@
+import { Button, TextField } from '@mui/material'
 import React from 'react'
-
+import './register.css'
+// import re
 function Register() {
   return (
-    <div>
+    <div className='container' >
         <h1>Register</h1>
-        <input data-testid='username'  placeholder='Enter Username'/>
-        <input data-testid='password' placeholder='Enter password'/>
-        <input data-testid='confirm-password' placeholder='Confirm the Password'/>
-        <input data-testid='gender' placeholder='Enter Gender'/>
-        <input data-testid='profile-picture' placeholder='Select the profile picture' type='file'/>
-        <input data-testid='email-address' placeholder='Enter Email Address'/>
+        <TextField id="outlined-basic" data-testid="username" label="Enter Username" variant="outlined" size={"small"} />
+        <TextField id="outlined-basic" data-testid="password" label="Enter Password" variant="outlined" size={"small"} className='input' />
+        <TextField id="outlined-basic" data-testid="confirm-password" label="Confirm The Password" variant="outlined" size={"small"} />
+        <TextField id="outlined-basic" data-testid="gender" label="Enter Gender" variant="outlined" size={"small"} />
+        <TextField id="outlined-basic" data-testid="email-address" label="Enter Email Address" variant="outlined" size={"small"} />
+        <TextField id="outlined-basic" data-testid="profile-picture"  variant="standard" size={"small"}  type='file'/>
+        <Button variant="contained">Register</Button>
     </div>
 
   )
