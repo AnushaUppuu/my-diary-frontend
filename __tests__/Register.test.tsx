26ba727ec159b4aa321e8e50445af7bd59129994
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-13 07:27:42 +0530

    Create tests for the register component

--- __tests__/Register.test.tsx
+++ __tests__/Register.test.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import Register from '../src/components/Register';
 import '@testing-library/jest-dom'
 describe("Register component",()=>{
     test("renders the elements correctly",()=>{
         render(<Register/>);
-        expect(screen.getByTestId('username')).toBeInTheDocument();
-        expect(screen.getByTestId('password')).toBeInTheDocument();
-        expect(screen.getByTestId('confirm-password')).toBeInTheDocument();
-        expect(screen.getByTestId('gender')).toBeInTheDocument();
+        expect(screen.getByLabelText('Enter Username')).toBeInTheDocument();
+        expect(screen.getByLabelText('Enter Password')).toBeInTheDocument();
+        expect(screen.getByLabelText('Confirm The Password')).toBeInTheDocument();
+        expect(screen.getByLabelText('Enter Gender')).toBeInTheDocument();
+        expect(screen.getByLabelText('Enter Email Address')).toBeInTheDocument();
         expect(screen.getByTestId('profile-picture')).toBeInTheDocument();
-        expect(screen.getByTestId('email-address')).toBeInTheDocument();
     })
+   
 })
