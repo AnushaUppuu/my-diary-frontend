da700b275c896ece4ba5786414bb9b27db77e0bd
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-13 07:28:11 +0530

    Modify the App tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../src/App';
 import '@testing-library/jest-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+jest.mock('react-router-dom', () => ({
+  BrowserRouter:jest.fn(),
+  Routes:jest.fn(),
+  Route:jest.fn(),
+}))
 test('renders learn react link', () => {
   render(<App />);
   expect(screen.getByText('Welcome to the MyDiary')).toBeInTheDocument()
