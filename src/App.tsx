cbcf9fce51d66cece395a5341d680b274294b75e
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-13 11:23:32 +0530

    Create styles for the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Register from './components/Register';
 import { Routes,Route, BrowserRouter, useNavigate } from 'react-router-dom';
-
+import Login from './components/login/Login';
+import './App.css'
 function App() {
   return (
-    <div>
+    <div className='mainContainer'>
       <h1>Welcome to the MyDiary</h1>
     <Routes>
     <Route path='/' element={ <Register/> } />
+    <Route path='/login' element={<Login/>}/>
     </Routes>
     </div>
  
