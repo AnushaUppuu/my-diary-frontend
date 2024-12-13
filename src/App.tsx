1a0d7ebd717209423c81457b3b36bd54594c8044
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-13 13:49:48 +0530

    Format the code lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import Register from './components/Register';
-import { Routes,Route, BrowserRouter, useNavigate } from 'react-router-dom';
-import Login from './components/login/Login';
-import './App.css'
+import React from "react";
+import Register from "./components/register/Register";
+import { Routes, Route, BrowserRouter, useNavigate } from "react-router-dom";
+import Login from "./components/login/Login";
+import "./App.css";
 function App() {
   return (
-    <div className='mainContainer'>
+    <div className="mainContainer">
       <h1>Welcome to the MyDiary</h1>
-    <Routes>
-    <Route index path='/'  element={ <Register/> } />
-    <Route path='/login' element={<Login/>}/>
-    </Routes>
+      <Routes>
+        <Route index path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
- 
   );
 }
 
