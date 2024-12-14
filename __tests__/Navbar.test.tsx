1fca3992b0787aa88a59d76ca2b38aa01bde9360
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-15 00:27:51 +0530

    Create tests for the Navbar

---
+++ __tests__/Navbar.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "../src/components/navbar/Navbar";
+describe("Navbar component", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
+        <Navbar />
+      </BrowserRouter>
+    );
+  });
+  test("should renders the elements correctly", () => {
+    expect(screen.getByRole("button", { name: "Home" })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Create" })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Profile" })).toBeInTheDocument();
+    expect(screen.getAllByRole("listitem").length).toBeLessThan(5);
+  });
+});
