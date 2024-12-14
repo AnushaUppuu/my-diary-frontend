59ce1d0b5f2a8e65bd5ae04cbfa185195232755d
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-15 00:30:26 +0530

    Formal the code lines

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,18 +1,19 @@
-import { render, screen } from "@testing-library/react"
-import { BrowserRouter } from "react-router-dom"
-import Home from "../src/components/Dashboard/Home"
-import React from 'react'
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Home from "../src/components/dashboard/Home";
+import React from "react";
 import "@testing-library/jest-dom";
-describe("Home component",()=>{
-    beforeEach(()=>{
-        render(
-            <BrowserRouter  future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-            <Home/>
-            </BrowserRouter>
-        )
-    })
-    it("should renders the elements correctly",()=>{
-        expect(screen.getByText("Home")).toBeInTheDocument();
-    })
-    
-})
+describe("Home component", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
+        <Home />
+      </BrowserRouter>
+    );
+  });
+  it("should renders the elements correctly", () => {
+    expect(screen.getByText("Home")).toBeInTheDocument();
+  });
+});
