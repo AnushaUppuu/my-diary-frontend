9cf60c720838abe46eafdd214269647ec854cc0e
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-16 01:00:00 +0530

    Update the home component , by calling fetching and rendering the response

--- src/components/Dashboard/Home.tsx
+++ src/components/Dashboard/Home.tsx
@@ -1,12 +1,35 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import "./Home.css";
 import Navbar from "../navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import { Authentication } from "../../context/Authentication";
+import { Diary } from "../../types/Diary";
+import SingleDiary from "../diary/SingleDiary";
 function Home() {
-  const { token } = useContext(Authentication);
+  const { token,username } = useContext(Authentication);
   const navigate = useNavigate();
+  const [diaries,setDiaries]=useState<Diary[]>([])
+  useEffect(()=>{
+    async function diariesFetch(){
+      const result=await fetch(`http://localhost:4000/users/diaries/${username}`,{
+        method:"GET",
+        headers:{
+          "Content-Type":"application/json",
+          "authorization":`Bearer ${token}`
+        }
+      })
+      if(result.ok){
+        const value=await result.json();
+        console.log(value);
+        setDiaries(value);
+      }
+    }
+    if(token){
+      diariesFetch();
+    }
+
+  },[])
   return (
     <div className="homeContainer">
       {!token && (
@@ -21,6 +44,13 @@ function Home() {
         <div>
           <Navbar />
           <Outlet />
+          {diaries && 
+          diaries.map((data)=>(
+            <div className="dairies-Box">
+            <SingleDiary  key={data.title} diary={data}/>
+            </div>
+          ))
+          }
         </div>
       )}
     </div>
