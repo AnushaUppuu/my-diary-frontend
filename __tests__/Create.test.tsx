b48b24f84353909233843d930712e8a3a8137d26
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-15 21:45:16 +0530

    Update tests for the create component

--- __tests__/Create.test.tsx
+++ __tests__/Create.test.tsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { act } from "@testing-library/react";
-import { fireEvent, render, screen} from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Create from "../src/components/diary/Create";
 import "@testing-library/jest-dom";
 import { BrowserRouter } from "react-router-dom";
 import { AuthenticationProvider } from "../src/context/Authentication";
 
-describe("Create component",()=>{
-    beforeEach(()=>{
-        render(
-            <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-            <AuthenticationProvider>
-                <Create/>
-            </AuthenticationProvider>
-            </BrowserRouter>
-        )
-       
-    })
-    it("Should render the elements correctly",()=>{
-        expect(screen.getByText("Create")).toBeInTheDocument();
-    })
-})
+describe("Create component", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
+        <AuthenticationProvider>
+          <Create />
+        </AuthenticationProvider>
+      </BrowserRouter>
+    );
+  });
+  it("Should render the elements correctly", () => {
+    expect(screen.getByText("Create a Diary")).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("Enter title")).toBeInTheDocument();
+    expect(screen.getAllByRole("radio").length).toBeLessThan(6);
+    expect(
+      screen.getByPlaceholderText("Enter the Message body")
+    ).toBeInTheDocument();
+  });
+});
