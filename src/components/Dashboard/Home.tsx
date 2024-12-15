f3de302d884ac43bd17bd6a10d2f2ca414dc1627
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-15 21:49:56 +0530

    Add the outlet so that it can render the child component

--- src/components/Dashboard/Home.tsx
+++ src/components/Dashboard/Home.tsx
@@ -2,19 +2,27 @@ import React, { useContext, useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import "./Home.css";
 import Navbar from "../navbar/Navbar";
+import { Outlet } from "react-router-dom";
 import { Authentication } from "../../context/Authentication";
 function Home() {
   const { token } = useContext(Authentication);
-  const navigate=useNavigate();
+  const navigate = useNavigate();
   return (
     <div className="homeContainer">
       {!token && (
         <div className="notLogin">
           <h1>Please Login or Register to access myDiary</h1>
-          <button className="goToMain" onClick={()=>navigate('/')}>Go to main</button>
+          <button className="goToMain" onClick={() => navigate("/")}>
+            Go to main
+          </button>
         </div>
       )}
-      {token && <Navbar />}
+      {token && (
+        <div>
+          <Navbar />
+          <Outlet />
+        </div>
+      )}
     </div>
   );
 }
