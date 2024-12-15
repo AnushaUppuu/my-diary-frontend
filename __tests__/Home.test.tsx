cf9bd169fa2e2a055d5c9c0aee43d3bd8e658c75
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-15 17:46:42 +0530

    Create tests case to check in case of the user not loged in

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,19 +1,51 @@
-import { render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 import Home from "../src/components/dashboard/Home";
 import React from "react";
 import "@testing-library/jest-dom";
+import { Authentication, AuthenticationProvider } from "../src/context/Authentication";
 describe("Home component", () => {
-  beforeEach(() => {
+  it("should renders the elements correctly", () => {
     render(
       <BrowserRouter
         future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
       >
-        <Home />
+        <AuthenticationProvider>
+          <Home />
+        </AuthenticationProvider>
       </BrowserRouter>
     );
+    expect(screen.getByRole("button", { name: "Go to main" })).toBeInTheDocument();
   });
-  it("should renders the elements correctly", () => {
-    expect(screen.getByText("Home")).toBeInTheDocument();
+  it("should go to the initial page upon clicking go to main button", async() => {
+    render(
+      <BrowserRouter
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      >
+        <AuthenticationProvider>
+          <Home />
+        </AuthenticationProvider>
+      </BrowserRouter>
+    );
+   await act(async()=>{
+     fireEvent.click(screen.getByRole('button',{name:"Go to main"}))
+   })
+   expect(window.location.pathname).toBe('/')
   });
+  it("Should renders the navbar and content upon login",()=>{
+    const token="token";
+    const setToken=jest.fn();
+    const username="something";
+    const setUsername=jest.fn();
+    render(
+      <BrowserRouter
+        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      > 
+        <Authentication.Provider value={{username,setUsername,token,setToken}}>
+          <Home />
+        </Authentication.Provider>
+      </BrowserRouter>
+    );
+    expect(screen.getByRole('button',{name:"Home"})).toBeInTheDocument();
+  })
 });
