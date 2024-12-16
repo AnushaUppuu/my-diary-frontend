520eb186fa178b425f985d3438f5959eac5c367e
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-16 13:35:28 +0530

    Create tests for the new home component

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,95 +1,57 @@
 import { act, fireEvent, render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import Home from "../src/components/dashboard/Home";
+import Dashboard from "../src/components/dashboard/Dashboard";
 import React from "react";
 import "@testing-library/jest-dom";
-import { Authentication, AuthenticationProvider } from "../src/context/Authentication";
-
-describe("Home component", () => {
+import {
+  Authentication,
+  AuthenticationProvider,
+} from "../src/context/Authentication";
+import Home from "../src/components/dashboard/Home";
+global.fetch = jest.fn(() =>
+    Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({ success: true }),
+    })
+  ) as jest.Mock;
+describe("Home component",()=>{
   
-  it("should renders the elements correctly", () => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        ok: false,
-        json: () => Promise.reject({ success: true }),
-      })
-    ) as jest.Mock;
-    render(
-      <BrowserRouter
-        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-      >
-        <AuthenticationProvider>
-          <Home />
-        </AuthenticationProvider>
-      </BrowserRouter>
-    );
-    expect(screen.getByRole("button", { name: "Go to main" })).toBeInTheDocument();
-  });
-  it("should go to the initial page upon clicking go to main button", async() => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        ok: false,
-        json: () => Promise.reject({ success: true }),
-      })
-    ) as jest.Mock;
-    render(
-      <BrowserRouter
-        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-      >
-        <AuthenticationProvider>
-          <Home />
-        </AuthenticationProvider>
-      </BrowserRouter>
-    );
-   await act(async()=>{
-     fireEvent.click(screen.getByRole('button',{name:"Go to main"}))
-   })
-   expect(window.location.pathname).toBe('/')
-  });
-  it("Should renders the navbar and content upon login",()=>{
-    const token="token";
-    const setToken=jest.fn();
-    const username="something";
-    const setUsername=jest.fn();
-    const setDiaries=jest.fn();
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        ok: true,
-        json: () => Promise.resolve({ success: true }),
-      })
-    ) as jest.Mock;
-    render(
-      <BrowserRouter
-        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-      > 
-        <Authentication.Provider value={{username,setUsername,token,setToken}}>
-          <Home />
-        </Authentication.Provider>
-      </BrowserRouter>
-    );
-    expect(screen.getByRole('button',{name:"Home"})).toBeInTheDocument();
-  })
-  it("should call the fetch",()=>{
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        ok: true,
-        json: () => Promise.resolve({ success: true }),
-      })
-    ) as jest.Mock;
-    const token="token";
-    const setToken=jest.fn();
-    const username="something";
-    const setUsername=jest.fn();
-    const setDiaries=jest.fn();
-    render(
-      <BrowserRouter
-        future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-      > 
-        <Authentication.Provider value={{username,setUsername,token,setToken}}>
-          <Home />
-        </Authentication.Provider>
-      </BrowserRouter>
-    );
-    expect(global.fetch).toHaveBeenCalledTimes(1);
-  })
-});
+    it("Should call the fetch",async()=>{
+        const token = "token";
+        const setToken = jest.fn();
+        const username = "something";
+        const setUsername = jest.fn();
+        const setDiaries=jest.fn();
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+              ok: true,
+              json: () => Promise.resolve({ success: true }),
+            })
+          ) as jest.Mock;
+        render(
+            <BrowserRouter  future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+            <Authentication.Provider value={{token,setToken,username,setUsername}}>
+                <Home/>
+            </Authentication.Provider>
+            </BrowserRouter>
+        )
+        expect(screen.getByText("All your diaries"));
+        expect(global.fetch).toHaveBeenCalledTimes(1);
+    })
+    it("Should not call the fetch",()=>{
+        global.fetch = jest.fn(() =>
+            Promise.resolve({
+              ok: true,
+              json: () => Promise.reject({ success: true }),
+            })
+          ) as jest.Mock;
+        render(
+            <BrowserRouter  future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+            <AuthenticationProvider>
+                <Home/>
+            </AuthenticationProvider>
+            </BrowserRouter>
+        )
+        expect(global.fetch).toHaveBeenCalledTimes(0);
+    })
+})
