19349d817940fa95fc0b792b5377360b968433cd
AnushaUppuu <anusha.uppu@everest.engineering>
2024-12-16 13:36:29 +0530

    Create home component

--- src/components/Dashboard/Home.tsx
+++ src/components/Dashboard/Home.tsx
@@ -1,58 +1,44 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import "./Home.css";
-import Navbar from "../navbar/Navbar";
-import { Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Authentication } from "../../context/Authentication";
 import { Diary } from "../../types/Diary";
 import SingleDiary from "../diary/SingleDiary";
+
 function Home() {
-  const { token,username } = useContext(Authentication);
+  const { token, username } = useContext(Authentication);
   const navigate = useNavigate();
-  const [diaries,setDiaries]=useState<Diary[]>([])
-  useEffect(()=>{
-    async function diariesFetch(){
-      const result=await fetch(`http://localhost:4000/users/diaries/${username}`,{
-        method:"GET",
-        headers:{
-          "Content-Type":"application/json",
-          "authorization":`Bearer ${token}`
+  const [diaries, setDiaries] = useState<Diary[]>([]);
+  useEffect(() => {
+    async function diariesFetch() {
+      const result = await fetch(
+        `http://localhost:4000/users/diaries/${username}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${token}`,
+          },
         }
-      })
-      if(result.ok){
-        const value=await result.json();
+      );
+      if (result.ok) {
+        const value = await result.json();
         console.log(value);
         setDiaries(value);
       }
     }
-    if(token){
+    if (token) {
       diariesFetch();
     }
-
-  },[])
+  }, []);
   return (
-    <div className="homeContainer">
-      {!token && (
-        <div className="notLogin">
-          <h1>Please Login or Register to access myDiary</h1>
-          <button className="goToMain" onClick={() => navigate("/")}>
-            Go to main
-          </button>
-        </div>
-      )}
-      {token && (
-        <div>
-          <Navbar />
-          <Outlet />
-          {diaries && 
-          diaries.map((data)=>(
-            <div className="dairies-Box">
-            <SingleDiary  key={data.title} diary={data}/>
-            </div>
-          ))
-          }
-        </div>
-      )}
+    <div>
+        <h1>All your diaries</h1>
+      {diaries &&
+        diaries.map((data) => (
+          <div className="dairies-Box">
+            <SingleDiary key={data.title} diary={data} />
+          </div>
+        ))}
     </div>
   );
 }
